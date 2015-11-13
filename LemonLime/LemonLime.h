4192b56839745e1d0e4faa084fb18dafae58d489
Kevin Wolkober <kevin.wolkober@gmail.com>
2015-11-13 17:01:43 +0800

    Greenfield

---
+++ LemonLime/LemonLime.h
@@ -0,0 +1,19 @@
+//
+//  LemonLime.h
+//  LemonLime
+//
+//  Created by Kevin Wolkober on 11/13/15.
+//  Copyright © 2015 Kevin Wolkober. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LemonLime.
+FOUNDATION_EXPORT double LemonLimeVersionNumber;
+
+//! Project version string for LemonLime.
+FOUNDATION_EXPORT const unsigned char LemonLimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LemonLime/PublicHeader.h>
+
+
